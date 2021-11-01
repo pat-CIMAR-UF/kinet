0533d35e0b2316f886cb24b4e5648328103e7511
Yiqun Wang <yiqunw0907@gmail.com>
2021-11-01 09:47:44 -0400

    Working version

--- example.py
+++ example.py
@@ -3,27 +3,20 @@ import time
 import sys
 import os
 
-'''
-Blue_1 10, 163, 207
-Blue_2 0, 101, 196
-Blue_3 0, 62, 120
-'''
-
 pds = PowerSupply("192.168.50.99")
 
 def ocean_cycle(pds, pause = 0.2):
     for i in range(175, 211) + range(210, 175, -1):
-        for k in range(45, 101, 1) + range(100, 45, -1):
+        for k in range(20, 80, 4) + range(80, 20, -4):
             hue = i / float(360.0)
             saturation = 0.95
             value = k / float(100.0)
-
             print(hue, saturation, value)
             for fixture in pds:
                 fixture.hsv = (hue, saturation, value)
             
             pds.go()
-            time.sleep(0.2)
+            time.sleep(0.12)
 
 def main():
     fix1 = FixtureRGB(0)
